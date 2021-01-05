25aa399af1524bd818a1e429491314c568f9bdcb
Bane3442 <52292310+Bane34@users.noreply.github.com>
2021-01-05 11:01:04 +0100

    Update minecraft_arduino.ino

--- arduino_code/minecraft_arduino.ino
+++ arduino_code/minecraft_arduino.ino
@@ -1,22 +1,22 @@
-#define LED 53
+#define LED 53 //Change this to your arduino PIN
 
-int byte_entrada = 0;
+int input_byte = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  byte_entrada = Serial.read();
   
-  if(byte_entrada == '0') {
+  input_byte = Serial.read();
+  
+  if(input_byte == '0') {
     digitalWrite(LED, LOW);
   }
-  else if(byte_entrada == '1'){
+  else if(input_byte == '1'){
     digitalWrite(LED, HIGH);
   }
   
